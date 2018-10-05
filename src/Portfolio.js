fc2aa575df9850c6d3318b0e28c560d5799cbeb4
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-10-05 16:59:54 -0700

    update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./Portfolio.css";
-import { MdArrowDropDown } from "react-icons/md";
+import { MdArrowDropDown, MdArrowDropUp, MdHome } from "react-icons/md";
 import resumePic from "./pictures/ResumePicCircle.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
+      <div className="main" id="home">
         <div className="background">
           <div className="NavBarContainer">
             <div class="menu-icon">
@@ -20,9 +20,12 @@ class App extends Component {
                 <a className="h3nav" href="#tech">
                   Skills
                 </a>
-                <a className="h3nav">Projects</a>
-                <a className="h3nav">Blog</a>
-                <a className="h3nav">Contact</a>
+                <a className="h3nav" href="#projects">
+                  Projects
+                </a>
+                <a className="h3nav" href="#contact">
+                  Contact
+                </a>
               </ul>
             </div>
           </div>
@@ -30,9 +33,7 @@ class App extends Component {
             <div className="content">
               <div className="content-container">
                 <ul className="content-list">
-                  <li className="content-world">
-                    Hello World
-                  </li>
+                  <li className="content-world">Hello World</li>
                 </ul>
               </div>
             </div>
@@ -48,13 +49,13 @@ class App extends Component {
         </div>
         <div className="about" id="about">
           <div className="about-me">
-            <h1>ABOUT</h1>
+            <h1>About</h1>
             <hr />
             <div className="about-contents">
               <img
                 src={resumePic}
                 className="profile-pic"
-                alt="profile-picture"
+                alt="resume"
               />
               <p className="about-text">
                 {/* I am a full stack software engineer in San Diego, CA. A veteran
@@ -70,12 +71,54 @@ class App extends Component {
             </div>
           </div>
           <div className="about-media">
-            <h1>MEDIA</h1>
+            <h1>Media</h1>
+            <div className="about-nb">
+              <a className="back" href="#home">
+                <MdArrowDropUp className="back-icon" />
+              </a>
+              <a className="next" href="#tech">
+                <MdArrowDropDown className="next-icon" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="skills" id="tech">
           <h1>Skills</h1>
+          <div className="next-back">
+            <a className="back" href="#about">
+              <MdArrowDropUp className="back-icon" />
+            </a>
+            <a className="next" href="#projects">
+              <MdArrowDropDown className="next-icon" />
+            </a>
+          </div>
         </div>
+        <div className="projects" id="projects">
+          <h1>Projects</h1>
+        </div>
+        <div className="blog" id="blog">
+          <h1>Blog</h1>
+          <div className="next-back">
+            <a className="back" href="#tech">
+              <MdArrowDropUp className="back-icon" />
+            </a>
+            <a className="next" href="#contact">
+              <MdArrowDropDown className="next-icon" />
+            </a>
+          </div>
+        </div>
+        <div className="contact" id="contact">
+          <h1>Contact</h1>
+          <div className="next-back">
+            <a className="next" href="#projects">
+              <MdArrowDropUp className="next-icon" />
+            </a>
+            <a className="home" href="#home">
+              <MdHome className="home-icon" />
+            </a>
+          </div>
+        </div>
+        <footer>Justin Faulkenberry 2018</footer>
       </div>
     );
   }
